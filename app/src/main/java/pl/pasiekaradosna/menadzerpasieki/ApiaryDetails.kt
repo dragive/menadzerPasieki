837a5f6d4987187efe1cf04d9443d01b06597d3b
Maciej Fender <maciejf@int.pl>
2021-12-18 22:27:16 +0100

    Dodanie nie działającego room controllera do bazy

--- app/src/main/java/pl/pasiekaradosna/menadzerpasieki/ApiaryDetails.kt
+++ app/src/main/java/pl/pasiekaradosna/menadzerpasieki/ApiaryDetails.kt
@@ -1,13 +1,47 @@
 package pl.pasiekaradosna.menadzerpasieki
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.room.Room
+import kotlinx.android.synthetic.main.activity_apiary_details.*
+import pl.pasiekaradosna.menadzerpasieki.dal.AppDatabase
+import pl.pasiekaradosna.menadzerpasieki.dal.User
 
 class ApiaryDetails : AppCompatActivity() {
+
+    private val TAG :String = "MenagerPasieki"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_apiary_details)
 
-        actionBar?.setHomeButtonEnabled(true);
+        actionBar?.setHomeButtonEnabled(true)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        try {
+            Log.e(TAG, "onStart: ",)
+            val db = AppDatabase.getInstance(this)
+
+            val userDao = db.userDao()
+
+            val user = User(1,"1","2");
+
+            val users = userDao.insertAll(user)
+
+            val tvDaoTest: TextView = findViewById(R.id.tvDaoTest)
+
+            tvDaoTest.text = users.toString()
+
+        }
+        catch (ex :Exception){
+            Log.e(TAG, "onStart: Błąd bazki: ",ex)
+        }
+
     }
 }
