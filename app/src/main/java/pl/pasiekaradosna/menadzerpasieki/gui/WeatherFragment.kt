d80f63a713fcd1534c7c458432797e786d70eecb
Maciej Fender <maciejf@int.pl>
2022-01-24 00:01:30 +0100

    Modyfikacja widoku pogody na home'ie

--- app/src/main/java/pl/pasiekaradosna/menadzerpasieki/gui/WeatherFragment.kt
+++ app/src/main/java/pl/pasiekaradosna/menadzerpasieki/gui/WeatherFragment.kt
@@ -1,10 +1,10 @@
 package pl.pasiekaradosna.menadzerpasieki.gui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import pl.pasiekaradosna.menadzerpasieki.R
 
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,6 +26,12 @@ class WeatherFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+//        https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=52.12257&lon=20.44369
     }
 
     override fun onCreateView(
